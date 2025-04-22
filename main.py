b2ea13fe1d66ca1b659e6a18d60f7c4c3b8e5815
Darshan <darshanp@vayavyalabs.com>
2025-04-22 11:39:39 +0530

    FEAT: Added main source code

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
+from graphviz import Digraph
 from processing import LoadHexArray, ProcessAndGenerateFlow
+import argparse
 
-# Get descriptors
-input_data = input("Enter Descriptor Bytes separated by spaces: ")
-descriptors = LoadHexArray(input_data)
+def main():
+    parser = argparse.ArgumentParser(description="Visualize USB descriptors")
+    parser.add_argument('--save', action='store_true', help="Save output as usb_descriptors.png")
+    parser.add_argument('--render', action='store_true', help="Render and display output")
+    args = parser.parse_args()
+    
+    # Get descriptors
+    input_data = input("Enter Descriptor Bytes separated by spaces: ")
+    descriptors = LoadHexArray(input_data)
 
-# Print debug descriptors
-flowDiagram = ProcessAndGenerateFlow(descriptors)
+    dot = Digraph()  # Prepare an instance
 
+    dot.clear()  # Clear previous nodes
+    # Print debug descriptors
+    dot = ProcessAndGenerateFlow(descriptors)
+    
+    # Check if passed through command line :)
+    if not (args.save or args.render):
+      # Prompt user for action
+      print("Choose an action:")
+      print("1. Save to file")
+      print("2. Render and view")
+      print("3. Save and render")
+      choice = input("Enter 1, 2, or 3: ")
+      if choice == "1":
+        dot.render('usb_descriptors', format='png', cleanup=True)
+        print("Saved as usb_descriptors.png")
+      elif choice == "2":
+          dot.render('usb_descriptors', format='png', view=True, cleanup=True)
+          print("Rendered and displayed")
+      elif choice == "3":
+          dot.render('usb_descriptors', format='png', view=True, cleanup=True)
+          print("Saved as usb_descriptors.png and displayed")
+      else:
+          print("Invalid choice, no action taken")
+        
+    # Perform actions based on arguments
+    if args.save and args.render:
+        dot.render('usb_descriptors', format='png', view=True, cleanup=True)
+        print("Saved as usb_descriptors.png and displayed")
+    elif args.save:
+        dot.render('usb_descriptors', format='png', cleanup=True)
+        print("Saved as usb_descriptors.png")
+    elif args.render:
+        dot.render('usb_descriptors', format='png', view=True, cleanup=True)
+        print("Rendered and displayed")
+        
 
+if __name__ == "__main__":
+    main()
--- processing.py
+++ processing.py
@@ -1,30 +1,27 @@
 from graphviz import Digraph
+from helpers import bcd_to_string, get_vendor_name, get_product_name, get_bos_device_capability
 
-from helpers import bcd_to_string, get_vendor_name, get_product_name
-
-dot = Digraph() # Prepare an instance
-  
 # Internal Functions
-def CreateDeviceDescriptorNode(descriptor:list):
-  '''**9.6.1 Device**: A device descriptor describes general information about a device. It includes information
-that applies globally to the device and all of the device’s configurations. A device has only
-one device descriptor.
-  '''
-  bLength = descriptor[0]
-  bDescriptorType = descriptor[1]
-  bcdUSB = bcd_to_string((descriptor[3] << 8) + descriptor[2])
-  bDeviceClass = descriptor[4]
-  bDeviceSubClass = descriptor[5]
-  bDeviceProtocol = descriptor[6]
-  bMaxPacketSize0 = descriptor[7]
-  idVendor = descriptor[8]
-  idProduct = (descriptor[9] << 8) + descriptor[10]
-  bcdDevice = bcd_to_string((descriptor[11] << 8) + descriptor[12])
-  iManufacturer = (descriptor[13] << 8) + descriptor[14]
-  iProduct = descriptor[15]
-  iSerialNumber = descriptor[16]
-  bNumConfigurations = descriptor[17]
-  return f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+def CreateDeviceDescriptorNode(descriptor: list):
+    '''**9.6.1 Device**: A device descriptor describes general information about a device. It includes information
+    that applies globally to the device and all of the device’s configurations. A device has only
+    one device descriptor.
+    '''
+    bLength = descriptor[0]
+    bDescriptorType = descriptor[1]
+    bcdUSB = bcd_to_string((descriptor[3] << 8) + descriptor[2])  # Little-endian
+    bDeviceClass = descriptor[4]
+    bDeviceSubClass = descriptor[5]
+    bDeviceProtocol = descriptor[6]
+    bMaxPacketSize0 = descriptor[7]
+    idVendor = (descriptor[9] << 8) + descriptor[8]  # Little-endian
+    idProduct = (descriptor[11] << 8) + descriptor[10]  # Little-endian
+    bcdDevice = bcd_to_string((descriptor[13] << 8) + descriptor[12])  # Little-endian
+    iManufacturer = descriptor[14]
+    iProduct = descriptor[15]
+    iSerialNumber = descriptor[16]
+    bNumConfigurations = descriptor[17]
+    return f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
 <TR><TD BGCOLOR="lightgrey"><B>Device Descriptor</B></TD></TR>
 <TR><TD>bLength:  {bLength}</TD></TR>
 <TR><TD>bDescriptorType:  {bDescriptorType}</TD></TR>
@@ -33,73 +30,215 @@ one device descriptor.
 <TR><TD>bDeviceSubClass:  {bDeviceSubClass}</TD></TR>
 <TR><TD>bDeviceProtocol:  {bDeviceProtocol}</TD></TR>
 <TR><TD>bMaxPacketSize0:  {bMaxPacketSize0}</TD></TR>
-<TR><TD>idVendor:  {idVendor} (${get_vendor_name(idVendor=idVendor)})</TD></TR>
-<TR><TD>idProduct:  {idProduct} (${get_product_name(idVendor=idVendor, idProduct=idProduct)})</TD></TR>
+<TR><TD>idVendor:  {idVendor} ({get_vendor_name(idVendor)})</TD></TR>
+<TR><TD>idProduct:  {idProduct} ({get_product_name(idVendor, idProduct)})</TD></TR>
 <TR><TD>bcdDevice:  {bcdDevice}</TD></TR>
 <TR><TD>iManufacturer:  {iManufacturer}</TD></TR>
 <TR><TD>iProduct:  {iProduct}</TD></TR>
 <TR><TD>iSerialNumber:  {iSerialNumber}</TD></TR>
 <TR><TD>bNumConfigurations:  {bNumConfigurations}</TD></TR>
 </TABLE>>'''
-  
-  
-def CreateConfigurationDescriptorNode(descriptor:list):
-  print("hello")
-  
-def CreateStringDescriptorNode(descriptor:list):
-  print("hello")
-  
-def CreateInterfaceDescriptorNode(descriptor:list):
-  print("hello")
-  
-def CreateEndpointDescriptorNode(descriptor:list):
-  print("hello")
-  
-def CreateInterfaceAssociationDescriptorNode(descriptor:list):
-  print("hello")
-  
-def CreateDeviceCapabilityDescriptorNode(descriptor:list):
-  print("hello")
-  
-def CreateSSEndpointCompanionDescriptorNode(descriptor:list):
-  print("hello")
-  
-def CreateSSPIsochEndpointCompanionDescriptorNode(descriptor:list):
-  print("hello")
-  
-def CreateBOSDescriptorNode(descriptor:list):
-  bLength
-  bDescriptorType
-  wTotalLength
-  bNumDeviceCaps
-  bLength
-  bDescriptorType
-  bDevCapabilityType
-  CapabilityDependent
+
+def CreateConfigurationDescriptorNode(descriptor: list):
+    '''**9.6.3 Configuration**: Describes a specific device configuration.'''
+    bLength = descriptor[0]
+    bDescriptorType = descriptor[1]
+    wTotalLength = (descriptor[3] << 8) + descriptor[2]  # Little-endian
+    bNumInterfaces = descriptor[4]
+    bConfigurationValue = descriptor[5]
+    iConfiguration = descriptor[6]
+    bmAttributes = descriptor[7]
+    bMaxPower = descriptor[8]
+    return f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+<TR><TD BGCOLOR="lightgrey"><B>Configuration Descriptor</B></TD></TR>
+<TR><TD>bLength:  {bLength}</TD></TR>
+<TR><TD>bDescriptorType:  {bDescriptorType}</TD></TR>
+<TR><TD>wTotalLength:  {wTotalLength}</TD></TR>
+<TR><TD>bNumInterfaces:  {bNumInterfaces}</TD></TR>
+<TR><TD>bConfigurationValue:  {bConfigurationValue}</TD></TR>
+<TR><TD>iConfiguration:  {iConfiguration}</TD></TR>
+<TR><TD>bmAttributes:  {bmAttributes}</TD></TR>
+<TR><TD>bMaxPower:  {bMaxPower}</TD></TR>
+</TABLE>>'''
+
+def CreateStringDescriptorNode(descriptor: list):
+    '''**9.6.7 String**: Contains a Unicode string or language ID array (if index 0).'''
+    bLength = descriptor[0]
+    bDescriptorType = descriptor[1]
+    string_data = bytes(descriptor[2:bLength]).decode('utf-16-le')
+    return f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+<TR><TD BGCOLOR="lightgrey"><B>String Descriptor</B></TD></TR>
+<TR><TD>bLength:  {bLength}</TD></TR>
+<TR><TD>bDescriptorType:  {bDescriptorType}</TD></TR>
+<TR><TD>String:  {string_data}</TD></TR>
+</TABLE>>'''
+
+def CreateInterfaceDescriptorNode(descriptor: list):
+    '''**9.6.5 Interface**: Describes a specific interface within a configuration.'''
+    bLength = descriptor[0]
+    bDescriptorType = descriptor[1]
+    bInterfaceNumber = descriptor[2]
+    bAlternateSetting = descriptor[3]
+    bNumEndpoints = descriptor[4]
+    bInterfaceClass = descriptor[5]
+    bInterfaceSubClass = descriptor[6]
+    bInterfaceProtocol = descriptor[7]
+    iInterface = descriptor[8]
+    return f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+<TR><TD BGCOLOR="lightgrey"><B>Interface Descriptor</B></TD></TR>
+<TR><TD>bLength:  {bLength}</TD></TR>
+<TR><TD>bDescriptorType:  {bDescriptorType}</TD></TR>
+<TR><TD>bInterfaceNumber:  {bInterfaceNumber}</TD></TR>
+<TR><TD>bAlternateSetting:  {bAlternateSetting}</TD></TR>
+<TR><TD>bNumEndpoints:  {bNumEndpoints}</TD></TR>
+<TR><TD>bInterfaceClass:  {bInterfaceClass}</TD></TR>
+<TR><TD>bInterfaceSubClass:  {bInterfaceSubClass}</TD></TR>
+<TR><TD>bInterfaceProtocol:  {bInterfaceProtocol}</TD></TR>
+<TR><TD>iInterface:  {iInterface}</TD></TR>
+</TABLE>>'''
+
+def CreateEndpointDescriptorNode(descriptor: list):
+    '''**9.6.6 Endpoint**: Describes an endpoint within an interface.'''
+    bLength = descriptor[0]
+    bDescriptorType = descriptor[1]
+    bEndpointAddress = descriptor[2]
+    bmAttributes = descriptor[3]
+    wMaxPacketSize = (descriptor[5] << 8) + descriptor[4]  # Little-endian
+    bInterval = descriptor[6]
+    return f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+<TR><TD BGCOLOR="lightgrey"><B>Endpoint Descriptor</B></TD></TR>
+<TR><TD>bLength:  {bLength}</TD></TR>
+<TR><TD>bDescriptorType:  {bDescriptorType}</TD></TR>
+<TR><TD>bEndpointAddress:  {bEndpointAddress}</TD></TR>
+<TR><TD>bmAttributes:  {bmAttributes}</TD></TR>
+<TR><TD>wMaxPacketSize:  {wMaxPacketSize}</TD></TR>
+<TR><TD>bInterval:  {bInterval}</TD></TR>
+</TABLE>>'''
+
+def CreateInterfaceAssociationDescriptorNode(descriptor: list):
+    '''**9.6.4 Interface Association**: Groups interfaces that form a single function.'''
+    bLength = descriptor[0]
+    bDescriptorType = descriptor[1]
+    bFirstInterface = descriptor[2]
+    bInterfaceCount = descriptor[3]
+    bFunctionClass = descriptor[4]
+    bFunctionSubClass = descriptor[5]
+    bFunctionProtocol = descriptor[6]
+    iFunction = descriptor[7]
+    return f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+<TR><TD BGCOLOR="lightgrey"><B>Interface Association Descriptor</B></TD></TR>
+<TR><TD>bLength:  {bLength}</TD></TR>
+<TR><TD>bDescriptorType:  {bDescriptorType}</TD></TR>
+<TR><TD>bFirstInterface:  {bFirstInterface}</TD></TR>
+<TR><TD>bInterfaceCount:  {bInterfaceCount}</TD></TR>
+<TR><TD>bFunctionClass:  {bFunctionClass}</TD></TR>
+<TR><TD>bFunctionSubClass:  {bFunctionSubClass}</TD></TR>
+<TR><TD>bFunctionProtocol:  {bFunctionProtocol}</TD></TR>
+<TR><TD>iFunction:  {iFunction}</TD></TR>
+</TABLE>>'''
+
+def CreateDeviceCapabilityDescriptorNode(descriptor: list):
+    '''**9.6.2 Device Capability**: Describes capabilities within a BOS descriptor.'''
+    bLength = descriptor[0]
+    bDescriptorType = descriptor[1]
+    bDevCapabilityType = descriptor[2]
+    capability_name = get_bos_device_capability(bDevCapabilityType)
+    data_bytes = [hex(byte) for byte in descriptor[3:bLength]]
+    return f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+<TR><TD BGCOLOR="lightgrey"><B>Device Capability Descriptor</B></TD></TR>
+<TR><TD>bLength:  {bLength}</TD></TR>
+<TR><TD>bDescriptorType:  {bDescriptorType}</TD></TR>
+<TR><TD>bDevCapabilityType:  {bDevCapabilityType} ({capability_name})</TD></TR>
+<TR><TD>Capability Data:  {data_bytes}</TD></TR>
+</TABLE>>'''
+
+def CreateSSEndpointCompanionDescriptorNode(descriptor: list):
+    '''**SuperSpeed Endpoint Companion**: Additional descriptor for SuperSpeed endpoints.'''
+    bLength = descriptor[0]
+    bDescriptorType = descriptor[1]
+    bMaxBurst = descriptor[2]
+    bmAttributes = descriptor[3]
+    wBytesPerInterval = (descriptor[5] << 8) + descriptor[4]  # Little-endian
+    return f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+<TR><TD BGCOLOR="lightgrey"><B>SuperSpeed Endpoint Companion Descriptor</B></TD></TR>
+<TR><TD>bLength:  {bLength}</TD></TR>
+<TR><TD>bDescriptorType:  {bDescriptorType}</TD></TR>
+<TR><TD>bMaxBurst:  {bMaxBurst}</TD></TR>
+<TR><TD>bmAttributes:  {bmAttributes}</TD></TR>
+<TR><TD>wBytesPerInterval:  {wBytesPerInterval}</TD></TR>
+</TABLE>>'''
+
+def CreateSSPIsochEndpointCompanionDescriptorNode(descriptor: list):
+    '''**SuperSpeedPlus Isochronous Endpoint Companion**: For USB 3.1+ isochronous endpoints.'''
+    bLength = descriptor[0]
+    bDescriptorType = descriptor[1]
+    wReserved = (descriptor[3] << 8) + descriptor[2]  # Little-endian
+    dwBytesPerInterval = (descriptor[7] << 24) + (descriptor[6] << 16) + (descriptor[5] << 8) + descriptor[4]  # Little-endian
+    return f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+<TR><TD BGCOLOR="lightgrey"><B>SuperSpeedPlus Isochronous Endpoint Companion Descriptor</B></TD></TR>
+<TR><TD>bLength:  {bLength}</TD></TR>
+<TR><TD>bDescriptorType:  {bDescriptorType}</TD></TR>
+<TR><TD>wReserved:  {wReserved}</TD></TR>
+<TR><TD>dwBytesPerInterval:  {dwBytesPerInterval}</TD></TR>
+</TABLE>>'''
+
+def CreateBOSDescriptorNode(descriptor: list):
+    '''**9.6.2 BOS**: Binary Object Store descriptor, followed by capability descriptors.'''
+    bLength = descriptor[0]
+    bDescriptorType = descriptor[1]
+    wTotalLength = (descriptor[3] << 8) + descriptor[2]  # Little-endian
+    bNumDeviceCaps = descriptor[4]
+    return f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
+<TR><TD BGCOLOR="lightgrey"><B>BOS Descriptor</B></TD></TR>
+<TR><TD>bLength:  {bLength}</TD></TR>
+<TR><TD>bDescriptorType:  {bDescriptorType}</TD></TR>
+<TR><TD>wTotalLength:  {wTotalLength}</TD></TR>
+<TR><TD>bNumDeviceCaps:  {bNumDeviceCaps}</TD></TR>
+</TABLE>>'''
 
 # Exposed APIs
-
 def LoadHexArray(input_string):
     return [int(word, 16) for word in input_string.split() if word.startswith('0x')]
 
-def ProcessAndGenerateFlow(descriptors:list):
-  pass
-
-# Block 1: Rectangle with title and content
-dot.node('A', f'''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
-<TR><TD BGCOLOR="lightgrey"><B>Title 1</B></TD></TR>
-<TR><TD>Content 1{1}</TD></TR>
-<TR><TD>Content 2{1}</TD></TR>
-</TABLE>>''', shape='none')
-
-# Block 2: Same style, different data, title on top right
-dot.node('B', '''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
-<TR><TD BGCOLOR="lightgrey" ALIGN="RIGHT"><B>Title 2</B></TD></TR>
-<TR><TD>Content 2</TD></TR>
-</TABLE>>''', shape='none')
-
-# Connect blocks
-dot.edge('A', 'B')
-
-# Render flowchart
-dot.render('flowchart', format='png', view=True)
+def ProcessAndGenerateFlow(descriptors: list) -> Digraph:
+    dot = Digraph()  # Prepare an instance
+    dot.clear()  # Clear previous nodes
+    index = 0
+    prev_id = None
+    while index < len(descriptors):
+        bLength = descriptors[index]
+        if bLength == 0 or index + 1 >= len(descriptors):
+            break  # Safety check for invalid length or end of list
+        bDescriptorType = descriptors[index + 1]
+        if index + bLength > len(descriptors):
+            break  # Prevent slicing beyond list length
+        descriptor = descriptors[index:index + bLength]
+        if bDescriptorType == 1:  # Device Descriptor
+            table_str = CreateDeviceDescriptorNode(descriptor)
+        elif bDescriptorType == 2:  # Configuration Descriptor
+            table_str = CreateConfigurationDescriptorNode(descriptor)
+        elif bDescriptorType == 3:  # String Descriptor
+            table_str = CreateStringDescriptorNode(descriptor)
+        elif bDescriptorType == 4:  # Interface Descriptor
+            table_str = CreateInterfaceDescriptorNode(descriptor)
+        elif bDescriptorType == 5:  # Endpoint Descriptor
+            table_str = CreateEndpointDescriptorNode(descriptor)
+        elif bDescriptorType == 11:  # Interface Association Descriptor
+            table_str = CreateInterfaceAssociationDescriptorNode(descriptor)
+        elif bDescriptorType == 15:  # BOS Descriptor
+            table_str = CreateBOSDescriptorNode(descriptor)
+        elif bDescriptorType == 16:  # Device Capability Descriptor
+            table_str = CreateDeviceCapabilityDescriptorNode(descriptor)
+        elif bDescriptorType == 48:  # SuperSpeed Endpoint Companion
+            table_str = CreateSSEndpointCompanionDescriptorNode(descriptor)
+        elif bDescriptorType == 49:  # SuperSpeedPlus Isochronous Endpoint Companion
+            table_str = CreateSSPIsochEndpointCompanionDescriptorNode(descriptor)
+        else:
+            table_str = f"Unknown Descriptor Type: {bDescriptorType}"
+        node_id = f"desc_{index}"
+        dot.node(node_id, table_str, shape='none')
+        if prev_id is not None:
+            dot.edge(prev_id, node_id)
+        prev_id = node_id
+        index += bLength
+    dot.render('usb_descriptors', format='png', view=True)
